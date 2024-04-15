44c3b56e9b54fd17be605496128c6e91f75ebc6b
Pavel Beránek <pavelberanek91@gmail.com>
2024-04-15 11:40:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 def pozdrav(jak):
     print(jak)
 
+def kousej(koho):
+    print(f"kousam kous kous {koho}")
+
 pozdrav("Chro chro")
 pozdrav("kvaak kvaak")
 pozdrav("haf haf haf")
+kousej("beranka")
 
 # git commit -am "opravil jsem typo"
 # git push
