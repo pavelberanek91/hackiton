6196d2195e080d1f120b84411994e185d2e019db
pavelberanek91 <pavelberanek91@gmail.com>
2024-04-15 12:06:24 +0200

    test

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ def kousej(koho):
 def nacti_data():
     input("Zadej data: ")
 
+def nacti_cislo(): ... #tohle se mi nebude libit
+
+def nacti_slovo(): ... #tohle se mi bude libit
+
 pozdrav("Chro chro")
 pozdrav("kvaak kvaak")
 pozdrav("haf haf haf")
